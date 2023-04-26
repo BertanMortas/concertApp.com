110dd28b8a3d29bafbd2066fb34047b4b402dddc
BertanMortas <bertanmortas@gmail.com>
2023-04-26 09:36:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Search } from "./Components/search";
+import { ConcertAdd } from "./Components/ConcertAdd";
+import { useContext, useEffect } from "react";
+import ConcertContext from "./Components/concerts";
+
+function App() {
+  const { fetchConcerts, concerts } = useContext(ConcertContext);
+
+  useEffect(() => {
+    fetchConcerts();
+  }, []);
+
+  return (
+    <div>
+      <h1>hello world</h1>
+      <h1>concert app</h1>
+      <Search details={concerts} />
+      <ConcertAdd />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/concerts.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+const ConcertContext = createContext();
+function Provider({ children }) {
+  const [concerts, setConcerts] = useState([]);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("");
+  const [artistName, setArtistName] = useState("");
+  const [price, setPrice] = useState("");
+  const [url, setUrl] = useState(`https://picsum.photos/seed/123432/300/200`);
+  const [age, setAge] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const createConcert = async (
+    name,
+    location,
+    artistName,
+    price,
+    age,
+    date,
+    url
+  ) => {
+    const responce = await axios.post("http://127.0.0.1:3001/data", {
+      name: name,
+      location,
+      artistName,
+      price,
+      age,
+      date,
+      url,
+    });
+    const newConcerts = [...concerts, responce.data];
+    setConcerts(newConcerts);
+  };
+  const fetchConcerts = async () => {
+    const responce = await axios.get("http://127.0.0.1:3001/data");
+    setConcerts(responce.data);
+  };
+  const deleteConcerts = async (id) => {
+    await axios.delete(`http://127.0.0.1:3001/data/${id}`);
+    const updatedConcerts = concerts.filter((item) => {
+      return item.id !== id;
+    });
+    setConcerts(updatedConcerts);
+  };
+  const updateConcerts = async (
+    id,
+    newname,
+    newlocation,
+    newartistName,
+    newprice,
+    newage,
+    newdate,
+    newUrl
+  ) => {
+    const responce = await axios.put(`http://127.0.0.1:3001/data/${id}`, {
+      name: newname,
+      location: newlocation,
+      artistName: newartistName,
+      price: newprice,
+      age: newage,
+      date: newdate,
+      url: newUrl,
+    });
+    const updatedConcerts = concerts.map((data) => {
+      if (data.id === id) {
+        return { ...data, ...responce.data };
+      }
+      return data;
+    });
+    setConcerts(updatedConcerts);
+  };
+  const valuesToShare = {
+    concerts,
+    name,
+    setName,
+    date,
+    setDate,
+    location,
+    setLocation,
+    artistName,
+    price,
+    age,
+    url,
+    setUrl,
+    setArtistName,
+    setAge,
+    setPrice,
+    isOpen,
+    setIsOpen,
+    createConcert,
+    fetchConcerts,
+    deleteConcerts,
+    updateConcerts,
+  };
+  return (
+    <ConcertContext.Provider value={valuesToShare}>
+      {children}
+    </ConcertContext.Provider>
+  );
+}
+export { Provider };
+export default ConcertContext;
